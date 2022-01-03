330ca950416e4c67a6788841b8702808b5008feb
Ayman Patel <ayman.patel97@gmail.com>
2022-01-03 21:50:48 +0530

    feat: Add gitignore license git

--- node/twilio-blog/src/cli.js
+++ node/twilio-blog/src/cli.js
@@ -1,77 +1,70 @@
-import arg from "arg";
-import inquirer from "inquirer";
-import { createProject } from "./main";
+import arg from 'arg';
+import inquirer from 'inquirer';
+import { createProject } from './main';
 
-const parseArgumentIntoOptions = (rawArgs) => {
-
-    const args = arg(
-        {
-            '--git': Boolean,
-            '--yes': Boolean,
-            '--install': Boolean,
-            '-g': Boolean,
-            '-y': Boolean,
-            '-i': Boolean,     
-        },
-        {
-            argv: rawArgs.slice(2)
-        }
-    );
-
-    return {
-       skipPrompts: args['--yes'] || false,
-       git: args['--git'] || false,
-       template: args._[0],
-       runInstall: args['--install'] || false,
+function parseArgumentsIntoOptions(rawArgs) {
+  const args = arg(
+    {
+      '--git': Boolean,
+      '--yes': Boolean,
+      '--install': Boolean,
+      '-g': '--git',
+      '-y': '--yes',
+      '-i': '--install',
+    },
+    {
+      argv: rawArgs.slice(2),
     }
+  );
+  return {
+    skipPrompts: args['--yes'] || false,
+    git: args['--git'] || false,
+    template: args._[0],
+    runInstall: args['--install'] || false,
+  };
 }
 
-
-const promptForMissingOptions = async (options) => {
-
-    const defaultTemplate = "Javascript"
-
-    if(options.skipPrompts) {
-        return {
-            ...options,
-            template: options.template || defaultTemplate
-        }
-    }
-
-    
-    const questions = [];
-    if (!options.template) {
-    questions.push({
-        type: 'list',
-        name: 'template',
-        message: 'Please choose which project template to use',
-        choices: ['JavaScript', 'TypeScript'],
-        default: defaultTemplate,
-    });
-    }
-
-    if (!options.git) {
-    questions.push({
-        type: 'confirm',
-        name: 'git',
-        message: 'Initialize a git repository?',
-        default: false,
-    });
-    }
-
-    const answers = await inquirer.prompt(questions);
+async function promptForMissingOptions(options) {
+  const defaultTemplate = 'javascript';
+  if (options.skipPrompts) {
     return {
+      ...options,
+      template: options.template || defaultTemplate,
+    };
+  }
+
+  const questions = [];
+  if (!options.template) {
+    questions.push({
+      type: 'list',
+      name: 'template',
+      message: 'Please choose which project template to use',
+      choices: ['javascript', 'typescript'],
+      default: defaultTemplate,
+    });
+  }
+
+  if (!options.git) {
+    questions.push({
+      type: 'confirm',
+      name: 'git',
+      message: 'Should a git be initialized?',
+      default: false,
+    });
+  }
+
+  const answers = await inquirer.prompt(questions);
+  return {
     ...options,
     template: options.template || answers.template,
     git: options.git || answers.git,
-    };
-
+  };
 }
 
-export const cli = async (args) => {
-    let options = parseArgumentIntoOptions(args)
-    options = await promptForMissingOptions(options)
-    await createProject(options);
-    console.log(options)
-    
+export async function cli(args) {
+  let options = parseArgumentsIntoOptions(args);
+  options = await promptForMissingOptions(options);
+  await createProject(options);
 }
+
+// ...
